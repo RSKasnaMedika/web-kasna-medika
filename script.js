5f1f6b098b5e130fee81596eda473792586ccbe2
RSKasnaMedika <kasnamedikahospital@gmail.com>
2024-09-09 14:38:09 +0800

    update web

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
 // Chart.js Script
-const ctx = document.getElementById('qualityChart').getContext('2d');
-const qualityChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Kebersihan', 'Pelayanan', 'Keamanan', 'Fasilitas'],
-    datasets: [{
-      label: 'Skor Mutu',
-      data: [85, 90, 80, 75],
-      backgroundColor: ['#FFA601', '#CA3433', '#00FFFF', '#98FB98'],
-      borderColor: ['#FFA601', '#CA3433', '#00FFFF', '#98FB98'],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Document Loaded');
+  
+  const ctx = document.getElementById('qualityChart').getContext('2d');
+  const qualityChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Kebersihan', 'Pelayanan', 'Keamanan', 'Fasilitas'],
+      datasets: [{
+        label: 'Skor Mutu',
+        data: [85, 90, 80, 75],
+        backgroundColor: ['#FFA601', '#98FB98', '#FFA601', '#98FB98'],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
       }
     }
+  });
+  
+  // Hamburger Menu Script
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('#nav-links');
+  
+  if (hamburger) {
+    hamburger.addEventListener('click', () => {
+      hamburger.classList.toggle('active');
+      navLinks.classList.toggle('show');
+    });
+  } else {
+    console.log('Hamburger menu not found');
   }
-});
-
-// Hamburger Menu Script
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('#nav-links');
-
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navLinks.classList.toggle('show');
 });
