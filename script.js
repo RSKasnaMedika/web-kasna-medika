9fa683860a3f4b10b98b3bcd6cee37410f4384fd
RSKasnaMedika <kasnamedikahospital@gmail.com>
2024-09-09 13:13:20 +0800

    update 2

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log('Website loaded');
-});
+// Add interactivity here if needed
 
-document.addEventListener('DOMContentLoaded', function () {
-  var ctx = document.getElementById('qualityChart').getContext('2d');
-  var qualityChart = new Chart(ctx, {
-    type: 'bar', // tipe grafik: bar, line, pie, dll.
-    data: {
-      labels: ['Kebersihan', 'Pelayanan', 'Kualitas Perawatan', 'Fasilitas', 'Dokter'],
-      datasets: [{
-        label: 'Penilaian Mutu Rumah Sakit',
-        data: [85, 90, 80, 75, 95], // contoh data penilaian
-        backgroundColor: [
-          '#FF6347', // Warna baru untuk 'Kebersihan' (Tomato)
-          '#4682B4', // Warna baru untuk 'Pelayanan' (Steel Blue)
-          '#FFD700', // Warna baru untuk 'Kualitas Perawatan' (Gold)
-          '#20B2AA', // Warna baru untuk 'Fasilitas' (Light Sea Green)
-          '#9370DB'  // Warna baru untuk 'Dokter' (Medium Purple)
-        ],
-        borderColor: [
-          '#FF4500', // Warna border untuk 'Kebersihan' (Orange Red)
-          '#1E90FF', // Warna border untuk 'Pelayanan' (Dodger Blue)
-          '#FFA500', // Warna border untuk 'Kualitas Perawatan' (Orange)
-          '#008B8B', // Warna border untuk 'Fasilitas' (Dark Cyan)
-          '#8A2BE2'  // Warna border untuk 'Dokter' (Blue Violet)
-        ],
-        borderWidth: 2 // Mengatur ketebalan border
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Website loaded');
+  });
+  document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('qualityChart').getContext('2d');
+    var qualityChart = new Chart(ctx, {
+      type: 'bar', // tipe grafik: bar, line, pie, dll.
+      data: {
+        labels: ['Kebersihan', 'Pelayanan', 'Kualitas Perawatan', 'Fasilitas', 'Dokter'],
+        datasets: [{
+          label: 'Penilaian Mutu Rumah Sakit',
+          data: [85, 90, 80, 75, 95], // contoh data penilaian
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)'
+          ],
+          borderWidth: 2
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
         }
       }
-    }
+    });
   });
-});
+  
